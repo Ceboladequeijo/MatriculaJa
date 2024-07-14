6e16d8b37cf829e5854974ee5d0409ed15ea3595
gabrielCP06 <pimentel.claudino@academico.ifpb.edu.br>
2024-07-14 12:53:32 -0300

    teste 1001

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static("public"))
 app.get("/home", (req, res) => {
   res.sendFile('index.html', {root:'public/html'})
 })
-//alteracoes de teste
+//alteracoes de teste2
 app.get("/escolas", (req, res) => {
   res.sendFile('info-escola.html', {root:'public/html'})
 })
